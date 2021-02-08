ef1982b7f8d5e99e23ea4995531fe0bfe10fcafb
ruybragafilho <ruybragafilho@gmail.com>
2021-02-08 16:13:52 -0300

    Usando array no heap

--- Partida.cpp
+++ Partida.cpp
@@ -1,2 +1,63 @@
 #include "Partida.hpp"
 #include "Jogador.hpp"
+
+
+
+Partida::Partida(int num_jogadores)  {
+
+    this->_num_jogadores = num_jogadores;
+
+    this->_jogadores = new Jogador[this->_num_jogadores];
+}
+
+
+Partida::~Partida()  {
+
+    this->_num_jogadores = 0;
+
+    delete[](this->_jogadores);
+}
+
+
+
+void Partida::addJogadorCarta(string nomeJogador, int numero_pontos_carta, string naipe)  {
+
+}
+	
+
+int Partida::getNumJogadores()  {
+
+}
+	
+    
+int Partida::getNumAtualJogadores()  {
+
+}
+	
+
+void Partida::setJogadores(Jogador* jogadores)  {
+
+}
+	
+
+Jogador* Partida::getJogadores()  {
+
+}
+
+
+Jogador Partida::getCampeao()  {
+
+}
+	
+
+Jogador* Partida::getJogadoresOrdenados()  {
+
+}
+	
+
+void Partida::imprimeJogadoresOrdenados()  {
+
+}
+
+
+
